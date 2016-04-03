dbacd5ce933534d1befb1a18be3755d1fe61274d
Khalid Omar Ali <khalid.ali@consultant.com>
2016-04-03 02:06:36 +0100

    implement map

--- higher-order.js
+++ higher-order.js
@@ -4,6 +4,12 @@ let animals = [{ name: "Tabby", species: "Cat"},
                {name: "Duffy", species: "Bird"},
                {name: "Tweety", species: "Bird"}];
 
+/* FILTER
+
+ Task 1 - Filter Birds from Animals
+
+*/
+
 // filter bird - 1st style
 animals.filter(c => c.species === "Bird");
 
@@ -11,7 +17,7 @@ animals.filter(c => c.species === "Bird");
 let isBird = c => c.species === "Bird";
 animals.filter(isBird);
 
-// loop - 3rd style
+// not functional - 3rd style
 let birds = [];
 for (var i = 0; i < animals.length; i += 1) {
     if (animals[i].species === "Bird") {
@@ -19,4 +25,19 @@ for (var i = 0; i < animals.length; i += 1) {
     }
 }
 
-// grab the name of animals
+/* MAP
+
+ Task 2 - Grab the name of animals
+ Use Map and show non-functional method too
+
+ */
+
+// map - 1st Style
+console.log(animals.map(c => c.name));
+
+// not functional - 2nd Style
+let names = [];
+for (var j = 0; j < animals.length; j += 1) {
+    names.push(animals[j].name);
+}
+console.log(names);
