131817a4575dd54409cbeaae95b3505fc6716666
Khalid Omar Ali <khalid.ali@consultant.com>
2016-04-05 00:30:38 +0100

    fix up splitting

--- reduce.js
+++ reduce.js
@@ -3,8 +3,7 @@
 let fs = require('fs');
 
 var result = fs.readFileSync("coffee.txt", "utf-8");
-result.trim()
-      .split("/n")
-      .map(c => c.split("\t"));
 
-console.log(result);
+console.log(result.trim()
+      .split("/n")
+            .map(c => c.split("\t")));
