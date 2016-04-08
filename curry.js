f1023bf32b34853afea4fdbc81a700db21ffe798
Khalid Omar Ali <khalid.ali@consultant.com>
2016-04-08 01:23:48 +0100

    add non-curried example

--- curry.js
+++ curry.js
@@ -1,10 +1,18 @@
 "use strict";
 
-// Currying refers to when multiple arguments are transformed into a series of functions with only one argument
+/*
 
-let lionKing =
+ Currying refers to when multiple arguments are transformed into a series of functions with only one argument
+
+*/
+
+// Say we have a function that takes multiple arguments
+const getLionKingInfo = (name, size, prey) => "The name of the lion who was King is " + name + ", the size is " + size + " and their favourite prey is " + prey;
+
+// Curried
+const getLionKingInfo =
         name =>
         size =>
         prey => "The name of the lion who was King is " + name + ", the size is " + size + " and their favourite prey is " + prey;
 
-console.log(lionKing("Simba")("125-kilos")("Gazelle"));
+console.log(getLionKingInfo("Simba")("125-kilos")("Gazelle"));
