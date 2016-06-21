f99b42c6901c8c5dd1aa49a983e74196319b60f3
Khalid Omar Ali <khalid.ali@consultant.com>
2016-06-21 21:20:34 +0100

    show an example of closure and tidy up code

--- closure.js
+++ closure.js
@@ -1,15 +1,21 @@
 // to understand closure you must first understand scoping and hoisting
 
 let name = "Michael Jackson";
-
 function showCelebrity() {
     console.log(name);
 }
-
 function showOrdinaryPersonName() {
     name = "Hassan Mannah";
     console.log(name);
 }
+showCelebrity(); // "Michael Jackson"
+showOrdinaryPersonName(); // "Hassan Mannah"
 
-showCelebrity();
-showOrdinaryPersonName();
+// closure is an inner function that has access to the outer functions' variables and parametres
+function showName(first, last) {
+    function makeFullName() {
+        return "Your name is " + first + last;
+    }
+    return makeFullName();
+}
+showName("Joe", "Bloggs"); // "Joe Bloggs"
