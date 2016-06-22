e41e74d1c15189e31976699db810716f45037c91
Khalid Omar Ali <khalid.ali@consultant.com>
2016-06-22 06:47:16 +0100

    fix up scoping examples and add two closures

--- src/closure.js
+++ src/closure.js
@@ -1,12 +1,11 @@
-// to understand closure you must first understand scoping and hoisting
-
+// to understand closures you must first understand scoping and hoisting
 let name = "Michael Jackson";
 function showCelebrity() {
-    console.log(name);
+    console.log(name); // "Michael Jackson"
 }
 function showOrdinaryPersonName() {
     name = "Hassan Mannah";
-    console.log(name);
+    console.log(name); // "Hassan Mannah"
 }
 showCelebrity(); // "Michael Jackson"
 showOrdinaryPersonName(); // "Hassan Mannah"
@@ -18,4 +17,15 @@ function showName(first, last) {
     }
     return makeFullName();
 }
-showName("Joe", "Bloggs"); // "Joe Bloggs"
+showName("Joe", "Bloggs"); // "Your name is Joe Bloggs"
+
+// closures have access to outer function's variables even after the outer function has returned
+function greeting(say) {
+    function greetFriend(name) {
+        return say + " " + name;
+    }
+    return greetFriend;
+}
+let greetRecurser = greeting("Never graduate"); // greeting has now returned
+// the closure is called and it has access to the greeting function's paremeters/variables
+greetRecurser("Goncalo"); // "Never graduate Goncalo"
