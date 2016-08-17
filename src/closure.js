4d743e0af9fa6cd390e5ed59a696525c799ebff3
Khalid Omar Ali <khalid.ali@consultant.com>
2016-08-17 03:00:12 +0100

    add an example of partially applied function

--- src/closure.js
+++ src/closure.js
@@ -1,31 +1,49 @@
+"use strict";
+
 // to understand closures you must first understand scoping and hoisting
-let name = "Michael Jackson";
+let name = "Michael Jordan";
 function showCelebrity() {
-    console.log(name); // "Michael Jackson"
+    console.log(name); // "Michael Jordan"
 }
 function showOrdinaryPersonName() {
-    name = "Hassan Mannah";
-    console.log(name); // "Hassan Mannah"
+    name = "Joe Bloggs";
+    console.log(name); // "Joe Bloggs"
 }
 showCelebrity(); // "Michael Jackson"
 showOrdinaryPersonName(); // "Hassan Mannah"
 
+// basic func to show full name
+const showFullName = (first, middle, last) => {
+    return "your name is " + first + " " + middle + " " + last;
+};
+// partially applied function
+const withFirstAndLastPartial = (first, last) => {
+    return middle =>  {
+        return showFullName(first, middle, last);
+    };
+};
+
+withMiddleName = withFirstAndLastPartial("Andy", "Liang");
+withMiddleName("D"); // Andy D Liang
+
 // closure is an inner function that has access to the outer functions' variables and parametres
-function showName(first, last) {
-    function makeFullName() {
-        return "Your name is " + first + last;
+const showName = (first, last) => {
+    function makeFullName(middle) {
+        return "Your name is " + first + " " + middle + " " + last;
     }
-    return makeFullName();
-}
-showName("Joe", "Bloggs"); // "Your name is Joe Bloggs"
+    return makeFullName;
+};
+const joeBlogsWithMiddleName = showName("Joe", "Bloggs");
+
+joeBlogsWithMiddleName("Tim"); //returns Joe Tim Bloggs
 
 // closures have access to outer function's variables even after the outer function has returned
-function greeting(say) {
+const greeting = say => {
     function greetFriend(name) {
         return say + " " + name;
     }
     return greetFriend;
-}
+};
 let greetRecurser = greeting("Never graduate"); // greeting has now returned
 // the closure is called and it has access to the greeting function's paremeters/variables
-greetRecurser("Goncalo"); // "Never graduate Goncalo"
+greetRecurser("Ginger"); // "Never graduate Ginger"
