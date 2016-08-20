86d75d299416cb74660bd2b96f8a70419b69c1f2
Khalid Omar Ali <khalid.ali@consultant.com>
2016-08-20 20:31:50 +0100

    remove clutter and declare variables with const

--- src/closure.js
+++ src/closure.js
@@ -1,29 +1,28 @@
 "use strict";
 
 // to understand closures you must first understand scoping and hoisting
-let name = "Michael Jordan";
+const name = "Michael Jordan";
 function showCelebrity() {
     console.log(name); // "Michael Jordan"
 }
+
 function showOrdinaryPersonName() {
     name = "Joe Bloggs";
     console.log(name); // "Joe Bloggs"
 }
+
 showCelebrity(); // "Michael Jackson"
 showOrdinaryPersonName(); // "Hassan Mannah"
 
 // basic func to show full name
-const showFullName = (first, middle, last) => {
-    return "your name is " + first + " " + middle + " " + last;
-};
+const showFullName = (first, middle, last) => "your name is " + first + " " + middle + " " + last;
+
 // partially applied function
 const withFirstAndLastPartial = (first, last) => {
-    return middle =>  {
-        return showFullName(first, middle, last);
-    };
+    return middle => showFullName(first, middle, last);
 };
 
-withMiddleName = withFirstAndLastPartial("Andy", "Liang");
+const withMiddleName = withFirstAndLastPartial("Andy", "Liang");
 withMiddleName("D"); // Andy D Liang
 
 // closure is an inner function that has access to the outer functions' variables and parametres
@@ -33,9 +32,10 @@ const showName = (first, last) => {
     }
     return makeFullName;
 };
+
 const joeBlogsWithMiddleName = showName("Joe", "Bloggs");
 
-joeBlogsWithMiddleName("Tim"); //returns Joe Tim Bloggs
+joeBlogsWithMiddleName("M"); // Joe M Blogs
 
 // closures have access to outer function's variables even after the outer function has returned
 const greeting = say => {
@@ -44,6 +44,7 @@ const greeting = say => {
     }
     return greetFriend;
 };
-let greetRecurser = greeting("Never graduate"); // greeting has now returned
+
+const greetArecurser = greeting("Never graduate"); // greeting has now returned
 // the closure is called and it has access to the greeting function's paremeters/variables
-greetRecurser("Ginger"); // "Never graduate Ginger"
+greetArecurser("Ginger"); // "Never graduate Ginger"
