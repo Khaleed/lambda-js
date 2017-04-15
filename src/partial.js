d57557f49855d2a6eb93743d99a0d95686a42aa8
Khalid <khalid@alum.recurse.com>
2017-04-15 02:57:13 +0100

    Finish off the partial examples

--- src/partial.js
+++ src/partial.js
@@ -1,11 +1,23 @@
 /*
- Partial takes a function and few args than normal and returns a new function that takes the rest of the args.
+ Partial takes a function f and fewer args than normal arguments to f,
+ and returns a new function that takes variable number of additional args.
+ When called, the return function f with args and additional args.
  */
 
 const partial = (f, ...init) => (...rest) => f(...init, ...rest);
 
 const add = (a, b, c) => a + b + c;
 
-const addTwo = partial(add, a, b);
+const addTwo = partial(add, 1, 2);
 
-console.log(addTwo(1, 2)); //=> 3
+const addThree = addTwo(3);
+
+console.log(addThree); //=> 6
+
+const multiply = (w, x, y, z) => w * x * y * z;
+
+const multiplyTwo = partial(multiply, 2, 4);
+
+const multiplyThree = multiplyTwo(3, 1);
+
+console.log(multiplyThree); //=> 24
