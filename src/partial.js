603f62b809588c19d939cc9e11621b2899d2c411
Khalid <khalid@alum.recurse.com>
2017-11-07 18:52:06 +0000

    Add a rudimentary currying example using a shopping cart

--- src/partial.js
+++ src/partial.js
@@ -20,4 +20,27 @@ const multiplyTwo = partial(multiply, 2, 4);
 
 const multiplyThree = multiplyTwo(3, 1);
 
-console.log(multiplyThree); //=> 24
+// console.log(multiplyThree); //=> 24
+
+/*
+Rudimentary Currying Example
+*/
+
+// catalogue of items
+const items = [{ name: "iPhoneX-64GB", price: 999 },
+               { name: "iPhoneX-256GB", price: 1499 }];
+
+// helpers
+const map = (fn) => (array) => array.map(fn);
+const pluck = (key) => (object) => object[key]; // lodash fn
+const loopThroughItems = map(pluck("price"));
+const multiplier = (x) => (y) => x * y;
+
+const discount = multiplier(0.95); // (0.95) => (y) => 0.95 * y;
+const tax = multiplier(1.20); // (1.20) => (y) => 1.20 * y;
+
+const finalPrice = items.map(pluck('price')) // [999, 1499];
+                        .map(discount) // [949.05, 1424.05];
+                        .map(tax); // [1138.86, 1708.86];
+
+console.log(`final prices for the iPhoneX are £ ${ finalPrice }`);
