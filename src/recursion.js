ba0904aeb57e62edcd669e0668c9d6f46cf3bafc
Khalid <khalid@alum.recurse.com>
2018-01-03 01:29:27 +0000

    Refactor tail-call optimised factorial

--- src/recursion.js
+++ src/recursion.js
@@ -1,21 +1,12 @@
 // Example of Tail Call Optimisation
-const factorial_inner = (n, acc) => {
-    // fail fast
-    if (n < 0) {
-        return "undefined";
+const factorial = (n, acc = 1) => {
     // base case
-    } else if (n === 0) {
+    if (n < 2) {
         return acc;
-    // recursive calls, reduce calls to base case
-    } else {
-        return factorial_inner(n - 1, acc * n);
     }
+    // recursive case
+    return factorial(n - 1, n * acc);
 };
-
-// console.log(factorial_inner(2, 1));
-// console.log(factorial_inner(3, 1));
-// console.log(factorial_inner(4, 1));
-// console.log(factorial_inner(5, 1));
 
 // simple recursion - countdown from 100 to 1
 const countDown = x => {
